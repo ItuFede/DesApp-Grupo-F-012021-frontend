1af61a1b19be265fba798d3d2b78732b5e5726ca
leodelgadodev <leodelgado.dev@gmail.com>
2021-07-05 00:26:25 -0300

    fix env

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,18 +1,5 @@
-// export const environment = {
-//     API_URL: 'https://desappgrupof.heroku.com/',
-//     FIREBASE: {
-//         apiKey: "AIzaSyCvNqYHiXurEqG8p5bc0TP_p1E2qBfDITM",
-//         authDomain: "re-sena-api.firebaseapp.com",
-//         projectId: "re-sena-api",
-//         storageBucket: "re-sena-api.appspot.com",
-//         messagingSenderId: "529686230364",
-//         appId: "1:529686230364:web:411ea83e1d9488e644bd99"
-//     },
-//     SWAGGER_URL: 'https://desappgrupof.herokuapp.com/swagger-ui.html'
-// }
-
 export const environment = {
-    API_URL: 'http://localhost:8080/',
+    API_URL: 'https://desappgrupof.heroku.com/',
     FIREBASE: {
         apiKey: "AIzaSyCvNqYHiXurEqG8p5bc0TP_p1E2qBfDITM",
         authDomain: "re-sena-api.firebaseapp.com",
@@ -21,5 +8,18 @@ export const environment = {
         messagingSenderId: "529686230364",
         appId: "1:529686230364:web:411ea83e1d9488e644bd99"
     },
-    SWAGGER_URL: 'http://localhost:8080/swagger-ui.html'
+    SWAGGER_URL: 'https://desappgrupof.herokuapp.com/swagger-ui.html'
 }
+
+// export const environment = {
+//     API_URL: 'http://localhost:8080/',
+//     FIREBASE: {
+//         apiKey: "AIzaSyCvNqYHiXurEqG8p5bc0TP_p1E2qBfDITM",
+//         authDomain: "re-sena-api.firebaseapp.com",
+//         projectId: "re-sena-api",
+//         storageBucket: "re-sena-api.appspot.com",
+//         messagingSenderId: "529686230364",
+//         appId: "1:529686230364:web:411ea83e1d9488e644bd99"
+//     },
+//     SWAGGER_URL: 'http://localhost:8080/swagger-ui.html'
+// }
