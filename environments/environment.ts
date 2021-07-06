e087416d135acdaffce2b3c73e46f2ce944eafbd
leodelgadodev <leodelgado.dev@gmail.com>
2021-07-06 11:32:54 -0300

    fix env

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'http://localhost:8080/',
+    API_URL: 'https://desappgrupof.herokuapp.com/',
     FIREBASE: {
         apiKey: "AIzaSyCvNqYHiXurEqG8p5bc0TP_p1E2qBfDITM",
         authDomain: "re-sena-api.firebaseapp.com",
@@ -8,5 +8,5 @@ export const environment = {
         messagingSenderId: "529686230364",
         appId: "1:529686230364:web:411ea83e1d9488e644bd99"
     },
-    SWAGGER_URL: ''
+    SWAGGER_URL: 'https://desappgrupof.herokuapp.com/swagger-ui.html'
 }
